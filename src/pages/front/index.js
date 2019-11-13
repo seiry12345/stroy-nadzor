d0af92cf27bc28a2025dd9ee453778cc81d41a22
seiry12345 <seiry@mail.ru>
2019-11-14 01:48:02 +0600

    footer + images

--- src/pages/front/index.js
+++ src/pages/front/index.js
@@ -4,13 +4,14 @@ import '../../assets/scss/main.scss';
 // page scss
 import '../../components/button/button.scss'
 import '../../components/header/header.scss'
-import '../../components/footer/footer.scss'
 import '../../components/build/build.scss'
 import '../../components/form/form.scss'
 import '../../components/order/order.scss'
 import '../../components/faq/faq.scss'
 import '../../components/principles/principles.scss'
 import '../../components/modal/modal.scss'
+import '../../components/footer/footer.scss'
+import '../../components/social/social.scss'
 
 // jquery
 import 'jquery/dist/jquery.min'
