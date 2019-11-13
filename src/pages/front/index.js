c5a5a53b1e9a50efbf5a6c5915c1eab6e9cd647a
seiry12345 <seiry@mail.ru>
2019-11-13 20:04:38 +0600

    order

--- src/pages/front/index.js
+++ src/pages/front/index.js
@@ -7,6 +7,7 @@ import '../../components/header/header.scss'
 import '../../components/footer/footer.scss'
 import '../../components/build/build.scss'
 import '../../components/form/form.scss'
+import '../../components/order/order.scss'
 
 // jquery
 import 'jquery/dist/jquery.min'
